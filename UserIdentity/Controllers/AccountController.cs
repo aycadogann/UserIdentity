4a56c39301d545818a1d64b4aab7f4cacd664f03
Ayca Dogan <dogan.aycaa@gmail.com>
2025-07-30 16:07:26 +0300

    Kullanici adi, parolasi, email için kisitlamalar uygulandi

--- UserIdentity/Controllers/AccountController.cs
+++ UserIdentity/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,7 @@ using UserIdentity.Models;
 
 namespace UserIdentity.Controllers
 {
+    [Authorize]
     public class AccountController : Controller
     {
         private UserManager<ApplicationUser> userManager;
@@ -17,20 +19,82 @@ namespace UserIdentity.Controllers
         {
             var userStore = new UserStore<ApplicationUser>(new IdentityDataContext());
             userManager = new UserManager<ApplicationUser>(userStore);
+
+            userManager.PasswordValidator = new CustomPasswordValidator()
+            {
+                RequireDigit =true,
+                RequiredLength=7,
+                RequireLowercase=true,
+                RequireUppercase=true,
+                RequireNonLetterOrDigit=true
+            };
+
+            userManager.UserValidator = new UserValidator<ApplicationUser>(userManager)
+            {
+                RequireUniqueEmail = true,
+                AllowOnlyAlphanumericUserNames=false
+            };
         }
 
-        // GET: Account
+
         public ActionResult Index()
         {
             return View();
         }
 
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.returnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [AllowAnonymous]
+        public ActionResult Login(LoginModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = userManager.Find(model.UserName, model.Password);
+                if (user == null)
+                {
+                    ModelState.AddModelError("", "Yanlış kullanıcı adı veya parola");
+                }
+                else
+                {
+                    var authManager = HttpContext.GetOwinContext().Authentication;
+
+                    var identity = userManager.CreateIdentity(user, "ApplicationCookie");
+                    var authProperties = new AuthenticationProperties()
+                    {
+                        IsPersistent = true
+                    };
+                    authManager.SignOut();
+                    authManager.SignIn(authProperties, identity);
+
+                    return Redirect(string.IsNullOrEmpty(returnUrl) ? "/" : returnUrl);
+                }
+
+            }
+            ViewBag.returnUrl = returnUrl;
+
+
+            return View(model);
+        }
+
+
+        [AllowAnonymous]
         public ActionResult Register()
         {
             return View();
         }
+
+
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [AllowAnonymous]
         public ActionResult Register(Register model)
         {
             if (ModelState.IsValid)
@@ -56,6 +120,14 @@ namespace UserIdentity.Controllers
             }
 
             return View(model);
+        }
+
+        public ActionResult Logout()
+        {
+            var authManager = HttpContext.GetOwinContext().Authentication;
+            authManager.SignOut();
+
+            return RedirectToAction("Login");
         }
     }
 }
--- UserIdentity/Controllers/HomeController.cs
+++ UserIdentity/Controllers/HomeController.cs
@@ -6,10 +6,16 @@ using System.Web.Mvc;
 
 namespace UserIdentity.Controllers
 {
+    
     public class HomeController : Controller
-    {
-        // GET: Home
+    {     
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult About()
         {
             return View();
         }
